87ce7e37905f902e4a84c0d42c0353f730f93f30
canaleagustina <agustina.canale@accenture.com>
2023-07-11 17:33:37 -0300

    cambios

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from '@shared/components/side-bar/side-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: SidebarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
