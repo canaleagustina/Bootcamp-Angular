7f020864ab554c1e58ef9e34c1b5d070e143ffce
canaleagustina <agustina.canale@accenture.com>
2023-07-12 14:03:36 -0300

    cambios

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent {
+export class TracksPageComponent implements OnInit{
+
+  mockTracksList = [
+    {
+      name:'BEBE OFICIAL'
+    },
+    {
+      name:'BEBE OFICIAL'
+    },
+    {
+      name:'BEBE OFICIAL'
+    }
+  ]
+  constructor(){}
+  ngOnInit(): void {}
 
 }
