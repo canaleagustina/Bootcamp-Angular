8c2825bbad2e0b279fdb3038f6557258b6554680
canaleagustina <agustina.canale@accenture.com>
2023-08-01 12:50:34 -0300

    search

--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -13,24 +13,10 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   searchTracks$(term: string): Observable<any> {
-    return this.http.get(`${this.URL}/tracks?src=${term}`).pipe(
-      map((dataRaw: any) => this.sortBySearchTerm(dataRaw.data, term))
-    );
-  }
-
-  private sortBySearchTerm(data: any[], term: string): any[] {
-    // Ordenar los resultados según la similitud con el término de búsqueda
-    return data.sort((a, b) => {
-      const aMatch = a.name.toLowerCase().includes(term.toLowerCase());
-      const bMatch = b.name.toLowerCase().includes(term.toLowerCase());
-
-      if (aMatch && !bMatch) {
-        return -1;
-      } else if (!aMatch && bMatch) {
-        return 1;
-      } else {
-        return a.name.localeCompare(b.name); // Ordenar alfabéticamente si no hay una diferencia en la similitud
-      }
-    });
+  return this.http.get(`${this.URL}/tracks/${term}`)
+    .pipe(
+      map((dataRaw:any)=> dataRaw.data.length > 0 ? dataRaw.data : {})
+    )
+    
   }
 }
