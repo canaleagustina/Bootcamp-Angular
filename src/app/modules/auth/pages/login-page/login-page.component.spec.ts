217b14a4ee55296fac290ca0c9999e4d9d548440
canaleagustina <agustina.canale@accenture.com>
2023-08-09 10:57:26 -0300

    seccion 11

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginPageComponent } from './login-page.component';
 import { AuthService } from '../../services/auth.service';  
+import { By } from '@angular/platform-browser';
 
 describe('AuthPageComponent', () => {
   let component: LoginPageComponent;
@@ -20,7 +21,25 @@ describe('AuthPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => { 
-    expect(component).toBeTruthy();
+  it('Deberia retornar invalidado ', () => { 
+
+    const mockCredentials = {
+      email:'0x0x0x0x',
+      password:'11'
+    }
+
+    const emailForm:any = component.formLogin.get('email')
+    const passwordForm:any = component.formLogin.get('password')
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    expect(component.formLogin.invalid).toBeTruthy();
   });
+
+  it('el boton deberia tener la palabra iniciar sesion',()=>{
+    const elementRef = fixture.debugElement.query(By.css('.form-action'))
+    const getInnerText = elementRef.nativeElement.innerText
+    expect(getInnerText).toEqual('Iniciar sesión')
+  })
 });
