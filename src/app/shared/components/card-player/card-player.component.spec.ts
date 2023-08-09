5539e98dedf932ad09e54563a1208e8ece08e281
canaleagustina <agustina.canale@accenture.com>
2023-08-09 12:48:02 -0300

    fixed

--- src/app/shared/components/card-player/card-player.component.spec.ts
+++ src/app/shared/components/card-player/card-player.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardPlayerComponent } from './card-player.component';
-import { ImgBrokenDirective } from '../../../shared/directives/img-broken.directive'; // Asegúrate de que la ruta sea correcta
+import { ImgBrokenDirective } from '../../../shared/directives/img-broken.directive'; 
 
 describe('CardPlayerComponent', () => {
   let component: CardPlayerComponent;
-  let fixture: ComponentFixture<CardPlayerComponent>;
+  let fixture: ComponentFixture<CardPlayerComponent>; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CardPlayerComponent, ImgBrokenDirective] // Asegúrate de declarar la directiva aquí
+      declarations: [CardPlayerComponent, ImgBrokenDirective] 
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardPlayerComponent);
+    fixture = TestBed.createComponent(CardPlayerComponent); 
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
